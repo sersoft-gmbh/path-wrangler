877967e7f47896e617019db34befd7b8d0866738
Florian Friedrich <ffried@me.com>
2021-04-27 16:56:55 +0200

    Add Swift 5.4 support

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["PathWrangler"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/apple/swift-algorithms", .upToNextMinor(from: "0.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
